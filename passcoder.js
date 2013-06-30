bb148f603450f6c1fec1e7ed1d5ada250dece621
Jacob Valenta <jacob.valenta@gmail.com>
2013-06-29 21:57:26 -0400

    Creates Passcoder.js

---
+++ passcoder.js
@@ -0,0 +1,50 @@
+$.fn.passcoder = function(options) {
+  var settings = $.extend({
+		characters: 4,
+		margin: 5,
+	}, options);
+	var input_width = (this.width() - (settings.margin * 2 * settings.characters)) / settings.characters;
+	var name = this.attr('name');
+	var type = this.attr('type');
+	var className = this.attr('class');
+	var rendered_html = '';
+	for (var i=0;i<settings.characters;i++){
+		rendered_html += '<input type="' + type + '" class="' + className + ' passcoder" type="text" maxlength="1" name="' + name + '-' + i + '" />';
+	}
+	var html = $.parseHTML(rendered_html);
+	$(html).each(function(){
+		$(this).css('width', input_width + 'px');
+		$(this).css('margin', settings.margin + 'px');
+		$(this).css('text-align', 'center');
+	});
+    this.replaceWith(html);
+	$('.passcoder').keydown(function(e){
+		if (e.which == 8){
+			e.preventDefault();
+			$(this).val('');
+			$(this).prevAll('.passcoder:first').focus();
+		}
+		else if ((e.which > 47 && e.which < 58) || (e.which > 96 && e.which < 106)){
+			if ($(this).val() != ''){
+				$(this).nextAll('.passcoder:first').focus();				
+			}
+			else{
+				e.preventDefault();
+				$(this).val(String.fromCharCode(e.which));
+				$(this).nextAll('.passcoder:first').focus();
+			}
+		}
+		else if (e.which == 13){
+		}
+		else{
+			e.preventDefault();
+		}
+	});
+};
+$.fn.getPasscode = function() {
+	var finalValue = '';
+	this.each(function(){
+		finalValue += this.value;
+	});
+	return finalValue;
+}
